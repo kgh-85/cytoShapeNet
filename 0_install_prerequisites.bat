8f4a9e983414646ec3cd1d7aef0e7155d8a80a79
Konrad Hinkelmann <konrad.hinkelmann@kinoheld.de>
2020-05-28 16:10:22 +0200

    vc installer

--- 0_install_prerequisites.bat
+++ 0_install_prerequisites.bat
@@ -4,6 +4,7 @@ echo ============================================================
 echo[
 echo The python environment lives in "\bin\python-3.7.7.amd64" and is fully portable
 echo We need to elevate the current shell for adding "%~dp0" to Windows Defender exclusion list for way faster processing
+echo VC_redist.x64.exe will be installed as  needed for tensorflow. A restart is not required.
 echo[
 
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
@@ -26,6 +27,10 @@ cd .
 python -m pip install --upgrade pip
 pip install numpy seaborn matplotlib keras sklearn tensorflow
 
+echo [
+echo VC_redist.x64.exe will be installed as  needed for tensorflow. A RESTART IS NOT REQUIRED!
+lib\VC_redist.x64.exe /install /passive /norestart
+echo[
 echo Finished setup
 echo[
 pause
