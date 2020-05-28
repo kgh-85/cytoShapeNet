25c31bbe94627466447250e8493fa87371407f41
Konrad Hinkelmann <konrad.hinkelmann@kinoheld.de>
2020-05-28 15:05:44 +0200

    windows defender changes

--- 0_install_prerequisites.bat
+++ 0_install_prerequisites.bat
@@ -5,18 +5,9 @@ echo[
 echo The python environment lives in "\bin\python-3.7.7.amd64" and is fully portable
 echo We need to elevate the current shell for adding "%~dp0" to Windows Defender exclusion list for way faster processing
 echo[
-pause
-
 
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-
-:: %~dp0 holds the directory of the scriptfile
-SET datapath=%~dp0
-
-:: Does %datapath have a trailing slash? If so remove it 
-IF %datapath:~-1%==\ SET datapath=%datapath:~0,-1%
-powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "%datapath%"
-
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "\"%~dp0""
 
 echo Upgrading pip...
 set WINPYDIRBASE=%~dp0\bin
